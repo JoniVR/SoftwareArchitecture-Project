b25c90bc78305ffbda9650b5495fcac1c4cfaf3a
JoniVR <joni.vanroost@student.kdg.be>
2018-09-27 14:49:48 +0200

    add Max cameraId for generation + test

--- simulator/src/main/java/be/kdg/simulator/generators/RandomMessageGenerator.java
+++ simulator/src/main/java/be/kdg/simulator/generators/RandomMessageGenerator.java
@@ -4,14 +4,25 @@ import be.kdg.simulator.model.CameraMessage;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 @Component
 public class RandomMessageGenerator implements MessageGenerator {
 
+    private int maxCameraId = 3;
+
     @Override
     public CameraMessage generate() {
 
-        return new CameraMessage(1, generateLicensePlate(), LocalDateTime.now());
+        return new CameraMessage(generateCameraId(), generateLicensePlate(), LocalDateTime.now());
+    }
+
+    public void setMaxCameraId(int maxCameraId) {
+        this.maxCameraId = maxCameraId;
+    }
+
+    public int getMaxCameraId() {
+        return maxCameraId;
     }
 
     // private functions
@@ -21,9 +32,21 @@ public class RandomMessageGenerator implements MessageGenerator {
         return String.format("%s-%s-%s", generateDigits(1), generateLetters(3), generateDigits(3));
     }
 
+    private int generateCameraId(){
+
+        Random r = new Random();
+        int lower = 1;
+        int upper = this.maxCameraId;
+        int result = r.nextInt(upper-lower) + lower;
+
+        return result;
+    }
+
     private String generateLetters(int amount) {
+
         String letters = "";
         int n = 'Z' - 'A' + 1;
+
         for (int i = 0; i < amount; i++) {
             char c = (char) ('A' + Math.random() * n);
             letters += c;
@@ -32,8 +55,10 @@ public class RandomMessageGenerator implements MessageGenerator {
     }
 
     private String generateDigits(int amount) {
+
         String digits = "";
         int n = '9' - '0' + 1;
+
         for (int i = 0; i < amount; i++) {
             char c = (char) ('0' + Math.random() * n);
             digits += c;
--- simulator/src/test/java/be/kdg/simulator/SimulatorApplicationTests.java
+++ simulator/src/test/java/be/kdg/simulator/SimulatorApplicationTests.java
@@ -1,6 +1,7 @@
 package be.kdg.simulator;
 
 import be.kdg.simulator.generators.MessageGenerator;
+import be.kdg.simulator.generators.RandomMessageGenerator;
 import be.kdg.simulator.model.CameraMessage;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,16 +19,38 @@ public class SimulatorApplicationTests {
     @Autowired //Field injection!
     private MessageGenerator messageGenerator;
 
+    //TODO: possibly move these tests to own "messageGeneratortests"?
+
+    /**
+     * Tests is the format of the license plates is correct.
+     */
     @Test
-    public void testRandomLicensePlate() {
+    public void testRandomLicensePlateFormat() {
 
         CameraMessage cameraMessage = messageGenerator.generate();
 
         // regex for EU plates
-        String expectedRegex = "^[0-9]{1}-[A-Z]{3}-[0-9]{3}$";
+        String expectedRegex = "^[0-9]-[A-Z]{3}-[0-9]{3}$";
 
         // Check if generated plate matches with regex
         Assert.assertTrue("License plate does not have the correct format.",
                 cameraMessage.getLicenseplate().matches(expectedRegex));
+    }
+
+    /**
+     * Tests if the camera ID is between the correct bounds.
+     */
+    @Test
+    public void testRandomLicensePlateCameraIdBounds(){
+
+        CameraMessage cameraMessage = messageGenerator.generate();
+
+        RandomMessageGenerator randomMessageGenerator = (RandomMessageGenerator) messageGenerator;
+
+        int maxCameraId = randomMessageGenerator.getMaxCameraId();
+        int camId = cameraMessage.getId();
+
+        Assert.assertTrue("Random cameraID does not fit correct bounds.",
+                (camId <= maxCameraId) && camId > 0);
     }
 }
