7a977f6f757012f4e63f6c8e0a294df1741f35b3
JoniVR <joni.vanroost@student.kdg.be>
2018-10-14 15:22:17 +0200

    fix travis build

--- processor/src/main/java/be/kdg/processor/model/fine/Fine.java
+++ processor/src/main/java/be/kdg/processor/model/fine/Fine.java
@@ -2,16 +2,13 @@ package be.kdg.processor.model.fine;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
 @Entity
+@AllArgsConstructor
 public class Fine {
 
     @Id
