31e084a28818a4e66cb46786fc29352de1638f58
JoniVR <joni.vanroost@student.kdg.be>
2018-10-15 20:12:51 +0200

    add FineServiceTests.testSaveFine()

---
+++ processor/src/test/java/be/kdg/processor/service/FineServiceTests.java
@@ -0,0 +1,26 @@
+package be.kdg.processor.service;
+
+import be.kdg.processor.model.fine.Fine;
+import be.kdg.processor.model.fine.FineType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FineServiceTests {
+
+    @Autowired
+    private FineService fineService;
+
+    @Test
+    public void testSaveFine() {
+
+        Fine fine = new Fine( 10.0, FineType.EMISSION, true, "Dit is een test.", "1-ABC-123");
+        Fine savedFine = fineService.save(fine);
+        Assert.assertNotNull("Fine id is null", savedFine.getId());
+    }
+}
