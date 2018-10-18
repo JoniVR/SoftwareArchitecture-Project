a1ffb39248e1fd7d347bfaeba1404843e3725fb5
JoniVR <joni.vanroost@student.kdg.be>
2018-10-18 21:42:56 +0200

    implement REST test

--- processor/src/test/java/be/kdg/processor/controllers/rest/FineControllerTests.java
+++ processor/src/test/java/be/kdg/processor/controllers/rest/FineControllerTests.java
@@ -1,15 +1,26 @@
 package be.kdg.processor.controllers.rest;
 
 
+import be.kdg.processor.model.fine.Fine;
+import be.kdg.processor.model.fine.FineDTO;
+import be.kdg.processor.model.fine.FineType;
+import be.kdg.processor.service.FineService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,13 +33,26 @@ public class FineControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    //TODO: fix test!
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private FineService fineService;
 
     @Test
     public void testUpdateFineApproved() throws Exception {
 
-        /*
-        FineDTO fineDTO = new FineDTO(10.0, FineType.EMISSION, false, null, "1-ABC-123", LocalDateTime.now());
+        /**
+         * TODO: find better solution!
+         * Momenteel steken we eerst een fine in de repo om te testen,
+         * wanneer de databank persistent blijft ipv in memory is deze oplossing niet ideaal (omdat we dan elke keer dat we testen dit object opnieuw toevoegen)
+         */
+
+        // setup data to change
+        Fine fine = new Fine(1L,10, FineType.EMISSION, false, null, "1-ABC-123", LocalDateTime.now());
+        fineService.save(fine);
+
+        FineDTO fineDTO = modelMapper.map(fine, FineDTO.class);
         String requestJSON = objectMapper.writeValueAsString(fineDTO);
 
         mockMvc.perform(put("/api/fines/{id}", 1L).param("isApproved", "true")
@@ -36,8 +60,5 @@ public class FineControllerTests {
                 .content(requestJSON))
                 .andExpect(status().isAccepted())
                 .andExpect(content().string(containsString("true")));
-        */
-
-        Assert.assertTrue(true);
     }
 }
