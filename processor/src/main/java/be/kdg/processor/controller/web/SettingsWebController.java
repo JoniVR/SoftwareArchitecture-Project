3de82c8ddd630f57ab609dab4f219ef2125aa1e0
JoniVR <joni.vanroost@student.kdg.be>
2018-11-03 14:15:32 +0100

    improve html

--- processor/src/main/java/be/kdg/processor/controller/web/SettingsWebController.java
+++ processor/src/main/java/be/kdg/processor/controller/web/SettingsWebController.java
@@ -7,6 +7,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
